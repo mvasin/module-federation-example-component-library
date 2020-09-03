2406d8eeb94d7a83b06e1a13faea829bfa93f5d3
Mikhail Vasin <12434833+mvasin@users.noreply.github.com>
2020-09-03 11:45:19 +0200

    tweak Vercel URL

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPl
 
 module.exports = (_, argv) => ({
   output: {
-    publicPath: argv.mode === "development" ? "http://localhost:8081/" : "https://module-federation-component-library.vercel.app",
+    publicPath: argv.mode === "development" ? "http://localhost:8081/" : "https://module-federation-example-component-library.vercel.app",
   },
 
   resolve: {
